3c6e8ed9d85a922b13e8d04eed58640170384c08
Loghman <loghman7979@gmail.com>
2025-02-25 22:33:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 name = 'me'
 last_name = 'you'
 
@@ -38,3 +40,64 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+from .form import ShopUserCreationForm, ShopUserChangeForm
+
+
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 1
+
+
+@admin.register(ShopUser)
+class ShopUserAdmin(UserAdmin):
+    ordering = ['phone']
+
+    add_form = ShopUserCreationForm
+    form = ShopUserChangeForm
+    model = ShopUser
+
+    list_display = ['phone', 'first_name', 'last_name', 'is_active', 'is_staff']
+    inlines = [AddressInline]
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ("Personal Info", {'fields': ('first_name', 'last_name')}),
+        ("Permissions", {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ("Important Dates", {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('phone', 'password1', 'password2')}),
+        ("Personal Info", {'fields': ('first_name', 'last_name')}),
+        ("Permissions", {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ("Important Dates", {'fields': ('last_login', 'date_joined')}),
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
